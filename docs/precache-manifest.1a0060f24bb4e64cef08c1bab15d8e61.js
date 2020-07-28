ca79e526cca46c90e61d728f0fdee6793503baff
Sebastián Moreno <morenosebastian117@gmail.com>
2020-07-28 10:42:04 +0100

    docs agregado

---
+++ docs/precache-manifest.1a0060f24bb4e64cef08c1bab15d8e61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f967284c42e468d179588aa9ac9a02a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "257929e650f407b8a7e7",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "3f7bb471d93e30f9fd21",
+    "url": "/static/css/main.168d7aa2.chunk.css"
+  },
+  {
+    "revision": "257929e650f407b8a7e7",
+    "url": "/static/js/2.42420450.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42420450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7bb471d93e30f9fd21",
+    "url": "/static/js/main.a562a911.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
